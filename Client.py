ddf16707c16dfc070805d70344d116d5f150105a
Per Mikkelsen <mikkelsen.p@gmail.com>
2016-03-25 23:16:23 +0100

    Added files via upload
    Added simple exception handling and some documentation

--- Client.py
+++ Client.py
@@ -4,13 +4,24 @@ import socket
 target_host = "www.google.com"
 target_port = 80
 
-
+host_ip = socket.gethostbyname(target_host)
+# Create the CP socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect((target_host, target_port))
+try:
+    # Connect to the host and port
+    client.connect((host_ip, target_port))
+except socket.error:
+    print("Failed to connect")
 
-client.send(bytes("GET / HTTP/1.1\r\nhost:google.com\r\n\r\n","UTF-8"))
+# This is the HTTP command for fetching the main page 
+message = "GET / HTTP/1.1\r\n\r\n"
 
-response = client.recv(4096)
-
-print(response)
+try:    
+    client.send(bytes(message, "UTF-8"))
+    response = client.recv(4096)    
+except socket.error:
+    print("Failed to send message")
+finally:
+    print(response)
+    client.close()
